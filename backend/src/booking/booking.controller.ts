f73470bfd53d186ad234f6d9faaa4ee88bb1a8a7
esteban_castañeda <esteban2306dd@gmail.com>
2025-12-01 18:39:11 -0500

    create booking service createBooking and ading in controller, make a commit general to migrate dir out onedrive

--- backend/src/booking/booking.controller.ts
+++ backend/src/booking/booking.controller.ts
@@ -1,35 +1,39 @@
-import { Controller, Get, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post } from '@nestjs/common';
+import { BookingService } from './booking.service';
+import { CreateBookingDto } from './dto/create-booking.dto';
 
 @Controller('/booking')
 
 export class BookingController {
 
-    @Get('/booking?date=YYYY-MM-DD')
+    constructor(private bookingService: BookingService) { }
+
+    @Get()
     GetBookingsByDate() {
         return 'Bookings for the date'
     }
 
-    @Get('/booking?status=pending')
+    @Get()
     GetPendingBookings() {
         return 'Pending bookings'
     }
 
-    @Post('/api/bookings')
-    createBooking() {
-        return 'Booking created!'
+    @Post('/create')
+    createBooking(@Body() dto: CreateBookingDto) {
+        return this.bookingService.createBooking(dto)
     }
 
-    @Post('api/bookings/:id/confirmation')
+    @Post('/:id/confirmation')
     uploadBookingConfirmation() {
         return 'Booking confirmation uploaded!'
     }
 
-    @Patch()
+    @Patch('/:id/cancel')
     cancelBooking() {
         return 'Booking cancelled!'
     }
 
-    @Patch()
+    @Patch('/:id/confirm')
     confirmBooking() {
         return 'Booking confirmed!'
     }
