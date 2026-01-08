7561525e9f0ca0f1191551858b4073b2b833e09d
Luis Esteban Castaneda Luque <estebancastaneda@MacBook-Air-de-Luis.local>
2026-01-07 19:23:55 -0500

    add conflict resolve

--- frontend/src/modules/booking/components/steps/stepService/AddServiceDialog.tsx
+++ frontend/src/modules/booking/components/steps/stepService/AddServiceDialog.tsx
@@ -98,7 +98,11 @@ export default function AddServiceDialog({ onSelect, selectedIds }: Props) {
                 disabled={isSelected}
                 onClick={() => {
                   onSelect(service);
+<<<<<<< HEAD
                   setOpen(false); // 👈 CIERRA el modal correctamente
+=======
+                  setOpen(false);
+>>>>>>> 8284525 (merge posteriori change)
                 }}
                 className={`
                   w-full rounded-lg px-4 py-3 text-left transition
